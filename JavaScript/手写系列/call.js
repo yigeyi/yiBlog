e72303aea9d04dd26816e58ac392c9055b566911
蔡奕俊 <yijun.cai@jimicn.com>
2021-02-07 00:25:46 +0800

    优化一下call逻辑

--- JavaScript/手写系列/call.js
+++ JavaScript/手写系列/call.js
@@ -1,10 +1,13 @@
 Function.prototype.Call = function(context){
-    const ctx = context || window;
-    //将当前被嗲用的啊方法定义在ctx.func上
+    //判断传入是否为null
+    //如果为null直接指向window
+    let ctx = context || window;
+    //将当前被调用的方法定义在ctx.func上
     //为了能以对象调用的形式绑定this
     ctx.func = this;
 
     //获取参数
+    //从参数第二位开始截取到最后一个参数
     const args = Array.from(arguments).slice(1);
 
     //以对象调用的行是调用func，此时this指向ctx,
